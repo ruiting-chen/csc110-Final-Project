adaf391604662107080ea2e09cf6133bcc573b80
ruiting-chen <51133017+ruiting-chen@users.noreply.github.com>
2020-12-10 16:08:25 -0500

    Update data_process.py

--- data_process.py
+++ data_process.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Any
 from bs4 import BeautifulSoup
 import requests
 from datapackage import Package
@@ -6,7 +6,7 @@ from datapackage import Package
 
 def process_sea_level_info() -> List[List[str]]:
     """Return a list of sea level information extracted from the website we found.
-    The url of the website is 'http://uhslc.soest.hawaii.edu/data/fd.html'"""
+    The url of the website is http://uhslc.soest.hawaii.edu/data/fd.html"""
     r = requests.get('http://uhslc.soest.hawaii.edu/data/fd.html').text
     soup = BeautifulSoup(r, features="html.parser")
     info = soup.find_all('td')
@@ -20,11 +20,14 @@ def process_sea_level_info() -> List[List[str]]:
     return info_lst
 
 
-def process_temperature_data() -> List[List[str]]:
+def process_temperature_data() -> List[List[Any]]:
+    """Return a list of Global component of Climate at a Glance (GCAG) data
+    extracted from the website we found.
+    The url of the website is 'http://uhslc.soest.hawaii.edu/data/fd.html'"""
     package = Package('https://datahub.io/core/global-temp/datapackage.json')
 
     # get monthly temperature csv data
     for resource in package.resources:
         if resource.descriptor['name'] == 'monthly_csv':
             temp_raw_lst = resource.read()
-            return [temp_raw_lst[i] for i in range(0, len(temp_raw_lst)) if i % 2 == 0]
+            return [temp_raw_lst[i][1:] for i in range(0, len(temp_raw_lst)) if i % 2 == 0]
