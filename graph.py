9f881144c4fa9297fcec2645c61a67402e6b7d73
david-wei-01001 <72998341+david-wei-01001@users.noreply.github.com>
2020-12-11 23:00:00 -0600

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,11 +1,4 @@
 import plotly.graph_objects as go
-import plotly.express as px
-import numpy as np
-import random
-from generates import GenerateStationAndSeaLevel
-from entities import SeaLevel, Station
-
-
 import datetime
 from generates import GenerateTemperature, GenerateStationAndSeaLevel
 from climate_sea_level_system import ClimateSeaLevelSystem
@@ -23,158 +16,111 @@ def generate_tempera():
 def generate_sea():
     generate_station.generate_all(system)
 
-dates = []
-generate_sea()
-x = []
-y = []
-colouur = []
-station_name = []
-min_mon = []
-max_mon = []
-for station in system.get_station():
-    if len(system.get_station()[station].sea_level.keys()) < 240:
-        continue
-    min_mon.append(min(system.get_station()[station].sea_level.keys()))
-    max_mon.append(max(system.get_station()[station].sea_level.keys()))
 
-max_month = min(max_mon)
-min_month = max(min_mon)
-num_station = len(system.get_station())
-
-for month in range((max_month - min_month).days // 30):
-    dates.append(month)
-    inner_lst = []
+def graph_data_set_up() -> tuple:
+    dates = []
+    generate_sea()
+    x = []
+    y = []
+    color = []
+    station_name = []
+    min_mon = []
+    max_mon = []
     for station in system.get_station():
-        if len(system.get_station()[station].sea_level.keys()) < 120:
+        if len(system.get_station()[station].sea_level.keys()) < 240:
             continue
-        lon = system.get_station()[station].location[0]
-        la = system.get_station()[station].location[1]
-        new_date = min_month + datetime.timedelta(month * 30)
-        day = datetime.date(new_date.year, new_date.month, 6)
-        if day not in system.get_station()[station].sea_level:
-            colour = 'yellow'
-        else:
-            if system.get_station()[station].sea_level[day] < base_height:
-                colour = 'blue'
+        min_mon.append(min(system.get_station()[station].sea_level.keys()))
+        max_mon.append(max(system.get_station()[station].sea_level.keys()))
+
+    max_month = min(max_mon)
+    min_month = max(min_mon)
+    num_station = len(system.get_station())
+
+    for month in range((max_month - min_month).days // 30):
+        dates.append(month)
+        inner_lst = []
+        for station in system.get_station():
+            if len(system.get_station()[station].sea_level.keys()) < 120:
+                continue
+            lon = system.get_station()[station].location[0]
+            la = system.get_station()[station].location[1]
+            new_date = min_month + datetime.timedelta(month * 30)
+            day = datetime.date(new_date.year, new_date.month, 6)
+            if day not in system.get_station()[station].sea_level:
+                colour = 'yellow'
             else:
-                colour = 'red'
-        x.append(la)
-        y.append(lon)
-        station_name.append(f'Name of station: {station}')
-        inner_lst.append(colour)
-    colouur.append(inner_lst)
+                if system.get_station()[station].sea_level[day] < base_height:
+                    colour = 'blue'
+                else:
+                    colour = 'red'
+            x.append(la)
+            y.append(lon)
+            station_name.append(f'Name of station: {station}')
+            inner_lst.append(colour)
+        color.append(inner_lst)
+    return (num_station, color, dates, x, y, station_name)
 
+def draw_figure(tup: tuple) -> None:
+    num_station, color, dates, x, y, station_name = tup
+    fig_dict = {
+        "data": [],
+        "layout": {},
+        "frames": []
+    }
 
+    sliders_dict = {
+        "active": 0,
+        "yanchor": "top",
+        "xanchor": "left",
+        "currentvalue": {
+            "font": {"size": 20},
+            "prefix": "Months Since Recorded:",
+            "visible": True,
+            "xanchor": "right"
+        },
+        "transition": {"duration": 300, "easing": "cubic-in-out"},
+        "pad": {"b": 10, "t": 50},
+        "len": 0.9,
+        "x": 0.1,
+        "y": 0,
+        "steps": []
+    }
 
-
-
-
-
-
-
-
-
-# fig = go.Figure(px.scatter_geo(lat=x, lon=y, color=[*colouur],
-#                             ))
-# fig.update_layout(height=1000, margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
-# fig.update_geos(showland=True, landcolor='Green',
-#                 showocean=True, oceancolor='LightBlue',
-#                 showrivers=True, rivercolor='DarkBlue',
-#                 showlakes=True, lakecolor='DarkBlue',
-#                showcountries=True, countrycolor='White')
-# fig.update_geos(lataxis_showgrid=True, lonaxis_showgrid=True)
-# # fig.update_layout(height=1000)
-# fig.show()
-#
-#
-# def see_detail(station: str):
-#     go_plot(station)
-#
-# name = input('Please type in the station name that you want to see detailed report of. Hover over it to see name.')
-# see_detail(name)
-
-
-
-
-
-
-
-
-
-
-# dates = ['2000', '2001', '2002', '2003', '2004']
-
-# make figure
-fig_dict = {
-    "data": [],
-    "layout": {},
-    "frames": []
-}
-
-# x = np.random.randint(-90, 90, 100)
-# y = np.random.randint(-180, 180, 100)
-
-# print(x)
-# print(y)
-
-# color = ['red', 'green', 'yellow', 'blue', 'black']
-
-# fig.update_layout(height=1000, margin={'r':0, 't':0, 'l':0, 'b':0})
-
-
-sliders_dict = {
-    "active": 0,
-    "yanchor": "top",
-    "xanchor": "left",
-    "currentvalue": {
-        "font": {"size": 20},
-        "prefix": "Year:",
-        "visible": True,
-        "xanchor": "right"
-    },
-    "transition": {"duration": 300, "easing": "cubic-in-out"},
-    "pad": {"b": 10, "t": 50},
-    "len": 0.9,
-    "x": 0.1,
-    "y": 0,
-    "steps": []
-}
-
-# make data
-fig = go.Scattergeo(lat=x[0:num_station], lon=y[0:num_station], mode='markers', marker={'color': colouur[0]},
-                    hovertext=station_name)
-print(x, y)
-fig_dict["data"].append(fig)
-
-# make frames
-for i in range(len(dates)):
-
-    fig = go.Scattergeo(lat=x[num_station * i: num_station * i + num_station],
-                        lon=y[num_station * i: num_station * i + num_station],
-                        mode='markers', marker={'color': colouur[i]},
+    fig = go.Scattergeo(lat=x[0:num_station], lon=y[0:num_station], mode='markers', marker={'color': color[0]},
                         hovertext=station_name)
-    frame = {"data": fig, 'name': dates[i]}
 
-    fig_dict["frames"].append(frame)
+    fig_dict["data"].append(fig)
 
-    slider_step = {"args": [[dates[i]],
-                            {"frame": {"duration": 300, "redraw": True},
-                             "mode": "immediate",
-                             "transition": {"duration": 300}}],
-                   "label": dates[i],
-                   "method": "animate"}
-    sliders_dict["steps"].append(slider_step)
+    # make frames
+    for i in range(len(dates)):
 
-fig_dict["layout"]["sliders"] = [sliders_dict]
+        fig = go.Scattergeo(lat=x[num_station * i: num_station * i + num_station],
+                            lon=y[num_station * i: num_station * i + num_station],
+                            mode='markers', marker={'color': color[i]},
+                            hovertext=station_name)
+        frame = {"data": fig, 'name': dates[i]}
 
-fig = go.Figure(fig_dict)
-# fig = go.Figure(fig)
-fig.show()
+        fig_dict["frames"].append(frame)
+
+        slider_step = {"args": [[dates[i]],
+                                {"frame": {"duration": 300, "redraw": True},
+                                "mode": "immediate",
+                                    "transition": {"duration": 300}}],
+                       "label": dates[i],
+                       "method": "animate"}
+        sliders_dict["steps"].append(slider_step)
+
+    fig_dict["layout"]["sliders"] = [sliders_dict]
+
+    fig = go.Figure(fig_dict)
+    # fig = go.Figure(fig)
+    fig.show()
 
 
 def see_detail(station: str):
     go_plot(station)
 
 
+draw_figure(graph_data_set_up())
 name = input('Please type in the station name that you want to see detailed report of. Hover over it to see name.')
 see_detail(name)
