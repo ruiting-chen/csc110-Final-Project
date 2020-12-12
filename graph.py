974cff93c7710640310c3c931d2914b90917fa88
ruiting-chen <51133017+ruiting-chen@users.noreply.github.com>
2020-12-12 11:46:10 -0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -8,7 +8,6 @@ from entities import Station
 system = ClimateSeaLevelSystem()
 generate_temp = GenerateTemperature()
 generate_station = GenerateStationAndSeaLevel()
-base_height = 900
 
 # import plotly.io as pio
 # pio.renderers.default = "browser"
@@ -29,6 +28,50 @@ def get_color(station: Station, height: float) -> str:
             return colors[i - 1]
 
 
+# def graph_data_set_up_old() -> tuple:
+#     generate_sea()
+#     dates = []
+#     x = []
+#     y = []
+#     color = []
+#     station_name = []
+#     min_mon = []
+#     max_mon = []
+#     for station in system.get_station():
+#         if len(system.get_station()[station].sea_level.keys()) < 240:
+#             continue
+#         min_mon.append(min(system.get_station()[station].sea_level.keys()))
+#         max_mon.append(max(system.get_station()[station].sea_level.keys()))
+#
+#     max_month = min(max_mon)
+#     min_month = max(min_mon)
+#     num_station = len(system.get_station())
+#
+#     for month in range((max_month - min_month).days // 30):
+#         dates.append(month)
+#         inner_lst = []
+#         for station in system.get_station():
+#             station_obj = system.get_station()[station]
+#             if len(station_obj.sea_level.keys()) < 120:
+#                 continue
+#             lon = station_obj.location[0]
+#             la = station_obj.location[1]
+#             new_date = min_month + datetime.timedelta(month * 30)
+#             day = datetime.date(new_date.year, new_date.month, 6)
+#             if day not in station_obj.sea_level:
+#                 colour = 'white'
+#             else:
+#                 colour = get_color(station_obj, station_obj.sea_level[day])
+#                 # print(colour)
+#             x.append(la)
+#             y.append(lon)
+#             station_name.append(f'Name of station: {station}')
+#             inner_lst.append(colour)
+#         color.append(inner_lst)
+#         # print(color)
+#     return (num_station, color, dates, x, y, station_name)
+
+
 def graph_data_set_up() -> tuple:
     generate_sea()
     dates = []
@@ -36,33 +79,27 @@ def graph_data_set_up() -> tuple:
     y = []
     color = []
     station_name = []
-    min_mon = []
-    max_mon = []
+
+    max_date_station = ''
+    max_date = datetime.date(1800, 1, 1)
     for station in system.get_station():
-        if len(system.get_station()[station].sea_level.keys()) < 240:
-            continue
-        min_mon.append(min(system.get_station()[station].sea_level.keys()))
-        max_mon.append(max(system.get_station()[station].sea_level.keys()))
+        if system.get_station()[station].max_date > max_date:
+            max_date_station = system.get_station()[station]
 
-    max_month = min(max_mon)
-    min_month = max(min_mon)
     num_station = len(system.get_station())
-
-    for month in range((max_month - min_month).days // 30):
-        dates.append(month)
+    date_list = sorted(max_date_station.sea_level)[-360:]
+    print(date_list)
+    for date in date_list:
+        dates.append(f'{date.year}-{date.month}')
         inner_lst = []
         for station in system.get_station():
             station_obj = system.get_station()[station]
-            if len(station_obj.sea_level.keys()) < 120:
-                continue
             lon = station_obj.location[0]
             la = station_obj.location[1]
-            new_date = min_month + datetime.timedelta(month * 30)
-            day = datetime.date(new_date.year, new_date.month, 6)
-            if day not in station_obj.sea_level:
+            if date not in station_obj.sea_level:
                 colour = 'white'
             else:
-                colour = get_color(station_obj, station_obj.sea_level[day])
+                colour = get_color(station_obj, station_obj.sea_level[date])
                 # print(colour)
             x.append(la)
             y.append(lon)
@@ -87,7 +124,7 @@ def draw_figure(tup: tuple) -> None:
         "xanchor": "left",
         "currentvalue": {
             "font": {"size": 20},
-            "prefix": "Months Since Recorded:",
+            "prefix": "Year and Month:",
             "visible": True,
             "xanchor": "right"
         },
